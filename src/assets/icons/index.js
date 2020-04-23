95ab33d30ab11ed115b67511d0f8aed8b02d1097
Jaênia <jaeniaps@gmail.com>
2020-04-23 09:44:04 -0300

    ref/ Adjust data according to api

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,7 +1,7 @@
-import organization from './organization.svg';
+import company from './organization.svg';
 import location from './location.svg';
-import star from './star.svg';
-import repositories from './repositories.svg';
+import starred from './star.svg';
+import publicRepos from './repositories.svg';
 import followers from './followers.svg';
 
-export default { organization, location, star, repositories, followers };
+export default { company, location, starred, publicRepos, followers };
--- src/components/UserDetails/index.js
+++ src/components/UserDetails/index.js
@@ -5,12 +5,20 @@ import { StyledList } from './styles';
 
 import icons from '../../assets/icons';
 
+const altTypes = {
+  company: 'Company',
+  location: 'Location',
+  starred: 'Number of stars',
+  publicRepos: 'Number of public repos',
+  followers: 'Number of followers',
+};
+
 export default function UserDetails({ details }) {
   return (
     <StyledList>
       {details.map((detail) => (
         <li data-testid="user-detail" key={detail.value}>
-          <img src={icons[detail.type]} alt={detail.type} />
+          <img src={icons[detail.type]} alt={altTypes[detail.type]} />
           {detail.value}
         </li>
       ))}
