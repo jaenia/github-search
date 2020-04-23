2290add2b81b75ea625b52683201ebadbfe50415
Jaênia <jaeniaps@gmail.com>
2020-04-23 16:28:29 -0300

    ref/ Adjust user and repositories mock

--- src/pages/Result/index.js
+++ src/pages/Result/index.js
@@ -9,20 +9,29 @@ import RepositoryList from '../../components/RepositoryList';
 
 const user = {
   name: 'Jaênia Sousa',
-  avatar_url: 'https://avatars2.githubusercontent.com/u/17907292?v=4',
   login: 'jaenia',
+  avatar_url: 'https://avatars2.githubusercontent.com/u/17907292?v=4',
+  details: {
+    company: 'The Galactic Empire',
+    location: 'Tatooine',
+    starred: 10,
+    publicRepos: 4,
+    followers: 100,
+  },
 };
 
 // const user = null;
 
 const repositories = [
   {
+    id: 1,
     name: 'github-search',
     description:
       'A simple app where you can search for GitHub users and view their repositories.',
     stargazers_count: 5,
   },
   {
+    id: 2,
     name: 'Death Star',
     description: 'The most powerful weapon in the Universe',
     stargazers_count: 10,
