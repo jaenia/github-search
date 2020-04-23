73fc41b90852e71e0e591a30b4bdcc7ac765b957
Jaênia <jaeniaps@gmail.com>
2020-04-23 09:47:32 -0300

    feature/ Add Result page

--- src/pages/Result/index.js
+++ src/pages/Result/index.js
@@ -1,13 +1,48 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { Container, UserNotFound } from './styles';
 
 import PageHeader from '../../components/PageHeader';
+import UserInfo from '../../components/UserInfo';
+
+import RepositoryList from '../../components/RepositoryList';
+
+const user = {
+  name: 'Jaênia Sousa',
+  avatar_url: 'https://avatars2.githubusercontent.com/u/17907292?v=4',
+  login: 'jaenia',
+};
+
+// const user = {null};
+
+const repositories = [
+  {
+    name: 'github-search',
+    description:
+      'A simple app where you can search for GitHub users and view their repositories.',
+    stargazers_count: 5,
+  },
+  {
+    name: 'Death Star',
+    description: 'The most powerful weapon in the Universe',
+    stargazers_count: 10,
+  },
+];
 
 export default function Result() {
   return (
     <Container>
       <PageHeader />
+      <main>
+        {user !== null ? (
+          <>
+            <UserInfo user={user} />
+            <RepositoryList repositories={repositories} />
+          </>
+        ) : (
+          <UserNotFound>User not found :(</UserNotFound>
+        )}
+      </main>
     </Container>
   );
 }
--- src/pages/Result/styles.js
+++ src/pages/Result/styles.js
@@ -1,7 +1,23 @@
 import styled from 'styled-components';
 
+import { colors } from '../../styles/global';
+
 export const Container = styled.div`
   max-width: 1020px;
   margin: 0 auto;
   padding: 1.5em;
+
+  main {
+    display: flex;
+    margin-top: 50px;
+  }
+`;
+
+export const UserNotFound = styled.span`
+  display: block;
+  padding-top: 124px;
+  width: 100%;
+  text-align: center;
+  font-size: 40px;
+  color: ${colors.lighterPurple};
 `;
