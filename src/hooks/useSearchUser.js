bfaf294d1a3b4884aecc50e9a30c1303bb42c0e4
Jaênia <jaeniaps@gmail.com>
2020-04-24 21:01:19 -0300

    ref/ Remove async syntax from useSearchUser

--- src/hooks/useSearchUser.js
+++ src/hooks/useSearchUser.js
@@ -2,23 +2,9 @@ import { useState, useCallback } from 'react';
 import { useHistory } from 'react-router-dom';
 
 import * as api from '../services/api';
+import { parseUser, parseRepos } from '../utils/parsers';
 
 import { errorMessages } from '../utils/types';
-
-function parseUser(user, stars) {
-  return {
-    name: user.name,
-    login: user.login,
-    avatar_url: user.avatar_url,
-    details: {
-      company: user.company.replace('@', ''),
-      location: user.location,
-      starred: stars,
-      publicRepos: user.public_repos,
-      followers: user.followers,
-    },
-  };
-}
 
 export default function useSearchUser() {
   const [search, setSearch] = useState('');
@@ -56,38 +42,31 @@ export default function useSearchUser() {
     [search, history]
   );
 
-  async function getSearch(searchText) {
+  function getSearch(searchText) {
     if (error.isError) setError({ message: '', isError: false });
     setLoading(true);
-    try {
-      const [user, repositories, stars] = await Promise.all([
-        api.getUser(searchText),
-        api.getRepos(searchText),
-        api.getUserStars(searchText),
-      ]);
 
-      const parsedUser = parseUser(user, stars);
-
-      const parsedRepos = repositories.map((repo) => ({
-        id: repo.id,
-        name: repo.name,
-        description: repo.description,
-        stargazers_count: repo.stargazers_count,
-      }));
-
-      setResult({ user: parsedUser, repos: parsedRepos });
-      setSearch('');
-      setLoading(false);
-    } catch (err) {
-      setError({
-        message:
-          err.message === 'Not Found'
-            ? errorMessages.notFound
-            : errorMessages.generic,
-        isError: true,
-      });
-      setLoading(false);
-    }
+    Promise.all([
+      api.getUser(searchText),
+      api.getRepos(searchText),
+      api.getUserStars(searchText),
+    ])
+      .then(([user, repositories, stars]) =>
+        setResult({
+          user: parseUser(user, stars),
+          repos: parseRepos(repositories),
+        })
+      )
+      .catch((err) =>
+        setError({
+          message:
+            err.message === 'Not Found'
+              ? errorMessages.notFound
+              : errorMessages.generic,
+          isError: true,
+        })
+      )
+      .finally(() => setLoading(false));
   }
 
   return {
@@ -95,7 +74,6 @@ export default function useSearchUser() {
     loading,
     result,
     error,
-    setSearch,
     handleInputChange,
     handleSubmit,
     getSearch,
