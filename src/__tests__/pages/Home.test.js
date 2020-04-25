2f218185b65c483302109f3d3e9e50849fd3b8cc
Jaênia <jaeniaps@gmail.com>
2020-04-24 21:14:49 -0300

    test/ Add mock for history push

--- src/__tests__/pages/Home.test.js
+++ src/__tests__/pages/Home.test.js
@@ -3,6 +3,14 @@ import { render } from '@testing-library/react';
 
 import Home from '../../pages/Home';
 
+jest.mock('react-router-dom', () => ({
+  useHistory: () => ({
+    history: {
+      push: jest.fn(),
+    },
+  }),
+}));
+
 describe('Home page', () => {
   it('should render h1 and search input', () => {
     const { container, getByTestId } = render(<Home />);
