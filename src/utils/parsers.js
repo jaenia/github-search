67c8f3af7f76a1cb628ac0d2956238cfac1944ef
Jaênia <jaeniaps@gmail.com>
2020-04-24 21:05:01 -0300

    feature/ Add parsers for user and repos

--- src/utils/parsers.js
+++ src/utils/parsers.js
@@ -4,3 +4,37 @@ export function getLastPageFromURL(url) {
     .match(/&page=[0-9]*/)[0]
     .split('=')[1];
 }
+
+export function parseUser(user, stars) {
+  return {
+    name: user.name,
+    login: user.login,
+    avatar_url: user.avatar_url,
+    details: {
+      company: user.company.replace('@', ''),
+      location: user.location,
+      starred: stars,
+      publicRepos: user.public_repos,
+      followers: user.followers,
+    },
+  };
+}
+
+export function parseRepos(repos) {
+  return repos
+    .map((repo) => ({
+      id: repo.id,
+      name: repo.name,
+      description: repo.description,
+      stargazers_count: parseInt(repo.stargazers_count, 10),
+    }))
+    .sort((a, b) => {
+      if (a.stargazers_count > b.stargazers_count) {
+        return -1;
+      }
+      if (a.stargazers_count < b.stargazers_count) {
+        return 1;
+      }
+      return 0;
+    });
+}
